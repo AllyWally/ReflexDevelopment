67df23db68fa5e15f21edc535c08b881496ad69e
Miles Rack <milesr4@protonmail.com>
2021-07-04 00:44:06 -0400

    Development version using bootstrap

--- js/contact-form.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#contact").addEventListener('submit', 
-(e) => {
-    e.preventDefault();
-    e.target.elements.name.value = ''
-    e.target.elements.email.value = ''
-    e.target.elements.subject.value = ''
-    e.target.elements.message.value = ''
-});
